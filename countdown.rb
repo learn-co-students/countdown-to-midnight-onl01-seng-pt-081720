f639333fdbc4ac5d829c93f74f08bdceb1773f7d
Brenda Ferrufino <ferrufinob@gmail.com>
2020-08-20 18:13:11 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -4,21 +4,23 @@ def countdown(number)
   while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
-  if number == 0
-     return "HAPPY NEW YEAR!"
-   end
+  # if number == 0
+  #   return "HAPPY NEW YEAR!"
+  # end
 end
+return "HAPPY NEW YEAR!"
 end
 
 def  countdown_with_sleep(number)
 while number > 0
-sleep 5
+sleep 1
 puts "#{number} SECOND(S)!"
 number -= 1
-if number == 0
+# if number == 0
+# return "HAPPY NEW YEAR!"
+# end
+end
 return "HAPPY NEW YEAR!"
-end
-end
 end
  
 
