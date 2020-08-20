bb899439f616b293ae47a193c81abd251356291f
Brenda Ferrufino <ferrufinob@gmail.com>
2020-08-20 22:39:30 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -4,9 +4,6 @@ def countdown(number)
   while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
-  # if number == 0
-  #   return "HAPPY NEW YEAR!"
-  # end
 end
 return "HAPPY NEW YEAR!"
 end
@@ -16,9 +13,6 @@ while number > 0
 sleep 1
 puts "#{number} SECOND(S)!"
 number -= 1
-# if number == 0
-# return "HAPPY NEW YEAR!"
-# end
 end
 return "HAPPY NEW YEAR!"
 end
